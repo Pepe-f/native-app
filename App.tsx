098b262659c2bc14ec780711f9df0c8d7aa1f974
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2024-04-26 23:33:16 +0300

    feat: error notification

--- App.tsx
+++ App.tsx
@@ -2,10 +2,19 @@ import { Image, StyleSheet, Text, View } from "react-native";
 import { Input } from "./shared/ui/Input/Input";
 import { Colors, Gaps } from "./shared/const/tokens";
 import { Button } from "./shared/ui/Button/Button";
+import { useState } from "react";
+import { ErrorNotification } from "./shared/ui/ErrorNotification/ErrorNotification";
 
 export default function App() {
+  const [error, setError] = useState<string | undefined>();
+
+  const alert = () => {
+    setError("Неверный логин или пароль");
+  };
+
   return (
     <View style={styles.container}>
+      <ErrorNotification error={error} />
       <View style={styles.content}>
         <Image
           source={require("./assets/logo.png")}
@@ -15,7 +24,7 @@ export default function App() {
         <View style={styles.form}>
           <Input placeholder="Email" />
           <Input placeholder="Пароль" isPassword />
-          <Button text="Войти" />
+          <Button text="Войти" onPress={alert} />
         </View>
         <Text>Восстановить пароль</Text>
       </View>
---
+++ shared/ui/ErrorNotification/ErrorNotification.tsx
@@ -0,0 +1,68 @@
+import { Animated, Dimensions, StyleSheet, Text } from "react-native";
+import { Colors, Fonts } from "../../const/tokens";
+import { useEffect, useState } from "react";
+
+interface ErrorNotificationProps {
+  error?: string;
+}
+
+export function ErrorNotification({ error }: ErrorNotificationProps) {
+  const [isShown, setIsShown] = useState<boolean>(false);
+  const animatedValue = new Animated.Value(-100);
+
+  const onEnter = () => {
+    Animated.timing(animatedValue, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  useEffect(() => {
+    if (!error) {
+      return;
+    }
+
+    setIsShown(true);
+
+    const timerId = setTimeout(() => {
+      setIsShown(false);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [error]);
+
+  if (!isShown) {
+    return null;
+  }
+
+  return (
+    <Animated.View
+      style={{
+        ...styles.error,
+        transform: [{ translateY: animatedValue }],
+      }}
+      onLayout={onEnter}
+    >
+      <Text style={styles.errorText}>{error}</Text>
+    </Animated.View>
+  );
+}
+
+const styles = StyleSheet.create({
+  error: {
+    position: "absolute",
+    width: Dimensions.get("screen").width,
+    backgroundColor: Colors.red,
+    padding: 15,
+    top: 50,
+  },
+  errorText: {
+    fontSize: Fonts.f16,
+    color: Colors.white,
+    textAlign: "center",
+    // fontFamily: Fonts.regular,
+  },
+});
