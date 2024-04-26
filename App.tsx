f46de57c0d378714bf600e903ff4c8046af3a58d
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2024-04-26 23:13:23 +0300

    feat: login form layout, input, button

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from "react-native";
+import { Input } from "./shared/ui/Input/Input";
+import { Colors, Gaps } from "./shared/const/tokens";
+import { Button } from "./shared/ui/Button/Button";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.content}>
+        <Image
+          source={require("./assets/logo.png")}
+          style={styles.logo}
+          resizeMode="contain"
+        />
+        <View style={styles.form}>
+          <Input placeholder="Email" />
+          <Input placeholder="Пароль" isPassword />
+          <Button text="Войти" />
+        </View>
+        <Text>Восстановить пароль</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +26,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    padding: 55,
+    backgroundColor: Colors.black,
+  },
+  content: {
+    alignItems: "center",
+    gap: Gaps.g50,
+  },
+  logo: {
+    width: 220,
+  },
+  form: {
+    alignSelf: "stretch",
+    gap: Gaps.g16,
   },
 });
