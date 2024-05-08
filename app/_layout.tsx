2e72c4fc5566982156bb5437831d57c71af4a09a
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2024-05-09 02:01:55 +0300

    feat: stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,16 @@
 import { Stack } from 'expo-router';
+import { Colors } from '../shared/const/tokens';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        statusBarColor: Colors.black,
+        contentStyle: { backgroundColor: Colors.black },
+      }}
+    >
+      <Stack.Screen name="index" />
+      <Stack.Screen name="restore" options={{ presentation: 'modal', headerShown: false }} />
+    </Stack>
+  );
 }
