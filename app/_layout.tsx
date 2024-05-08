151b48cedc0c46f3b1cd32b7bb7d5e9ae8bc3120
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2024-05-09 02:16:08 +0300

    feat: custom font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,10 +2,18 @@ import { Stack } from 'expo-router';
 import { Colors } from '../shared/const/tokens';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useFonts } from 'expo-font';
 
 export default function RootLayout() {
   const insets = useSafeAreaInsets();
+  const [loaded] = useFonts({
+    FiraSans: require('../assets/fonts/FiraSans-Regular.ttf'),
+    FiraSansSemiBold: require('../assets/fonts/FiraSans-SemiBold.ttf'),
+  });
 
+  if (!loaded) {
+    return null;
+  }
   return (
     <SafeAreaProvider>
       <StatusBar style="light" />
