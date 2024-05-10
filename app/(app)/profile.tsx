9bbc560874cfa2a9136a190dc6bfac476f5d9733
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2024-05-11 02:45:32 +0300

    feat: image uploader

--- app/(app)/profile.tsx
+++ app/(app)/profile.tsx
@@ -1,10 +1,28 @@
-import { View, Text } from 'react-native';
-import { Colors } from '../../shared/const/tokens';
+import { View, StyleSheet } from 'react-native';
+import { Gaps } from '../../shared/const/tokens';
+import { useState } from 'react';
+import { ImageUploader } from '../../shared/ui/ImageUploader/ImageUploader';
+import { Avatar } from '../../entities/user/ui/Avatar/Avatar';
 
 export default function Profile() {
+  const [image, setImage] = useState<string | null>(null);
+
   return (
     <View>
-      <Text style={{ color: Colors.white }}>Profile</Text>
+      <View style={styles.container}>
+        <Avatar image={image} />
+        <ImageUploader onUpload={setImage} onError={(e) => console.log(e)} />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    gap: Gaps.g20,
+    alignItems: 'center',
+    paddingHorizontal: 30,
+    paddingVertical: 20,
+  },
+});
