78f31eda235569e6af29e22d05724db5646e2a7e
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2024-05-10 12:13:37 +0300

    feat: redirect

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,24 +1,7 @@
 import { View, Text } from 'react-native';
 import { Colors } from '../../shared/const/tokens';
-import { useAtomValue } from 'jotai';
-import { authAtom } from '../../entities/auth/model/auth.state';
-import { useEffect } from 'react';
-import { router, useRootNavigationState } from 'expo-router';
 
 export default function MyCourses() {
-  const { access_token } = useAtomValue(authAtom);
-  const state = useRootNavigationState();
-
-  useEffect(() => {
-    if (!state.key) {
-      return;
-    }
-
-    if (!access_token) {
-      router.replace('/login');
-    }
-  }, [access_token, state]);
-
   return (
     <View>
       <Text style={{ color: Colors.white }}>MyCourses</Text>
