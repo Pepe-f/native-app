87e07542d86aa9344088ec0371568908238ad9f8
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2024-05-10 03:01:28 +0300

    feat: screen with local search params

---
+++ app/course/[alias].tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+import { useLocalSearchParams } from 'expo-router';
+import { Colors } from '../../shared/const/tokens';
+
+export default function CoursePage() {
+  const { alias } = useLocalSearchParams();
+
+  return (
+    <View>
+      <Text style={{ color: Colors.white }}>{alias}</Text>
+    </View>
+  );
+}
