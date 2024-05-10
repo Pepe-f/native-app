99588f4f26b442000ccb3b09e1cccada2e54034f
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2024-05-10 12:22:07 +0300

    feat: login, logout

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,10 +1,16 @@
 import { View, Text } from 'react-native';
 import { Colors } from '../../shared/const/tokens';
+import { Button } from '../../shared/ui/Button/Button';
+import { useSetAtom } from 'jotai';
+import { logoutAtom } from '../../entities/auth/model/auth.state';
 
 export default function MyCourses() {
+  const logout = useSetAtom(logoutAtom);
+
   return (
     <View>
       <Text style={{ color: Colors.white }}>MyCourses</Text>
+      <Button text="Выход" onPress={logout} />
     </View>
   );
 }
--- app/login.tsx
+++ app/login.tsx
@@ -2,26 +2,55 @@ import { Image, StyleSheet, View } from 'react-native';
 import { Input } from '../shared/ui/Input/Input';
 import { Colors, Gaps } from '../shared/const/tokens';
 import { Button } from '../shared/ui/Button/Button';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ErrorNotification } from '../shared/ui/ErrorNotification/ErrorNotification';
 import { CustomLink } from '../shared/ui/CustomLink/CustomLink';
+import { useAtom } from 'jotai';
+import { loginAtom } from '../entities/auth/model/auth.state';
+import { router } from 'expo-router';
 
 export default function App() {
-  const [error, setError] = useState<string | undefined>();
+  const [localError, setLocalError] = useState<string | undefined>();
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
 
-  const alert = () => {
-    setError('Неверный логин или пароль');
+  const [{ access_token, error, isLoading }, login] = useAtom(loginAtom);
+
+  const submit = () => {
+    if (!email) {
+      setLocalError('Не введен email');
+      return;
+    }
+
+    if (!password) {
+      setLocalError('Не введен пароль');
+      return;
+    }
+
+    login({ email, password });
   };
+
+  useEffect(() => {
+    if (error) {
+      setLocalError(error);
+    }
+  }, [error]);
+
+  useEffect(() => {
+    if (access_token) {
+      router.replace('/(app)');
+    }
+  }, [access_token]);
 
   return (
     <View style={styles.container}>
-      <ErrorNotification error={error} />
+      <ErrorNotification error={localError} />
       <View style={styles.content}>
         <Image source={require('../assets/logo.png')} style={styles.logo} resizeMode="contain" />
         <View style={styles.form}>
-          <Input placeholder="Email" />
-          <Input placeholder="Пароль" isPassword />
-          <Button text="Войти" onPress={alert} />
+          <Input placeholder="Email" onChangeText={setEmail} />
+          <Input placeholder="Пароль" isPassword onChangeText={setPassword} />
+          <Button text="Войти" onPress={submit} />
         </View>
         <CustomLink href={'/restore'} text="Восстановить пароль" />
       </View>
