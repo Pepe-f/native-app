b6bad0c60e836284612fce6b69d91486c09ce58b
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2024-05-11 03:05:35 +0300

    feat: sharing api

--- app/(app)/profile.tsx
+++ app/(app)/profile.tsx
@@ -1,4 +1,5 @@
 import { View, StyleSheet } from 'react-native';
+import * as Sharing from 'expo-sharing';
 import { Gaps } from '../../shared/const/tokens';
 import { useEffect, useState } from 'react';
 import { ImageUploader } from '../../shared/ui/ImageUploader/ImageUploader';
@@ -11,6 +12,12 @@ export default function Profile() {
   const [image, setImage] = useState<string | null>(null);
   const [profile, updateProfile] = useAtom(updateProfileAtom);
 
+  useEffect(() => {
+    if (profile && profile.profile?.photo) {
+      setImage(profile.profile?.photo);
+    }
+  }, [profile]);
+
   const submitProfile = () => {
     if (!image) {
       return;
@@ -18,29 +25,39 @@ export default function Profile() {
     updateProfile({ photo: image });
   };
 
-  useEffect(() => {
-    if (profile && profile.profile?.photo) {
-      setImage(profile.profile?.photo);
+  const shareProfile = async () => {
+    const isSharingAvailable = await Sharing.isAvailableAsync();
+
+    if (!isSharingAvailable) {
+      return;
     }
-  }, [profile]);
+
+    await Sharing.shareAsync('https://purpleschool.ru', {
+      dialogTitle: 'Поделиться профилем',
+    });
+  };
 
   return (
-    <View>
+    <View style={styles.wrapper}>
       <View style={styles.container}>
         <Avatar image={image} />
         <ImageUploader onUpload={setImage} onError={(e) => console.log(e)} />
       </View>
       <Button text="Сохранить" onPress={submitProfile} />
+      <Button text="Поделиться" onPress={shareProfile} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  wrapper: {
+    paddingVertical: 20,
+    paddingHorizontal: 30,
+    gap: Gaps.g20,
+  },
   container: {
     flexDirection: 'row',
     gap: Gaps.g20,
     alignItems: 'center',
-    paddingHorizontal: 30,
-    paddingVertical: 20,
   },
 });
